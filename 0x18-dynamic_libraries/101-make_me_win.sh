fb1798de3658677ca1bed913746574c3fde72894
11110000 10011111 10100110 10001010 <81971079+yayab0@users.noreply.github.com>
2023-10-16 06:15:08 +0200

    Delete 0x18-dynamic_libraries directory

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-wget -N -P /tmp https://raw.githubusercontent.com/nuuxcode/alx-low_level_programming/master/0x18-dynamic_libraries/randomx.so
-export LD_PRELOAD="/tmp/randomx.so"
--- 0x18-dynamic_libraries/README.md
+++
@@ -1,6 +0,0 @@
-Readme file
-
-gcc -c -fPIC mylib.c -o mylib.o
-gcc -shared mylib.o -o libmylib.so
-nm -D liball.so
-ldconfig
